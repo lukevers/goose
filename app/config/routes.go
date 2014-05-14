3a54c1fb9e2a81bdc8f70e491030c781261b4c69
Luke Evers <me@lukevers.com>
2014-05-14 07:43:59 -0400

    Better documentation

--- app/config/routes.go
+++ app/config/routes.go
@@ -29,6 +29,21 @@ type Route struct {
 | Application routes for the project should be
 | defined here.
 |
+| We are defining a slice of `Route`s and will make
+| sure that all routes defined here will be in the
+| project. `Type` can be one of the following:
+|
+|     GET, POST, PATCH, PUT, DELETE, OPTIONS
+|
+| `Path` is a [martini] route. Goose uses [martini] 
+| for routing. For a more comprehensive explanation
+| of routing possible, view their documentation.
+|
+|     https://github.com/go-martini/martini
+|
+| `Handler` is the func in our `app/handlers` folder
+| that handles the route and returns the view. 
+|
 */
 
 var Routes = []*Route{
